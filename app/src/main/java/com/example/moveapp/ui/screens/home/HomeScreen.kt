c0869d79a2013aed159743b09722321eff4439fe
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2024-09-20 10:54:45 +0200

    Added an easy navigation, that need further refactoring.

---
+++ app/src/main/java/com/example/moveapp/ui/screens/home/HomeScreen.kt
@@ -0,0 +1,38 @@
+package com.example.moveapp.ui.screens.home
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.moveapp.R
+import com.example.moveapp.ui.navigation.AppScreens
+import com.example.moveapp.ui.screens.login.LoginScreen
+
+@Composable
+fun HomeScreen(navController: NavController) {
+
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(
+            onClick = { navController.navigate(AppScreens.LOGIN.name) },
+            modifier = Modifier
+        ) {
+            LoginScreen();
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview(){
+    HomeScreen(rememberNavController())
+}
