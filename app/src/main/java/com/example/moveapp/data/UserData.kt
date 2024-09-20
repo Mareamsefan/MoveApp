4649aa5c2bbbf61b582def9d98c4a96e24bee10b
Vebhamnvik <vebjornhamnvik95@gmail.com>
2024-09-20 10:31:41 +0200

    Created enums used in dataclasses, initial draft of user and report data classes

--- app/src/main/java/com/example/moveapp/data/UserData.kt
+++ app/src/main/java/com/example/moveapp/data/UserData.kt
@@ -1,4 +1,30 @@
 package com.example.moveapp.data
 
-class UserData {
-}
+import com.example.moveapp.utility.UserTypeEnum
+import com.google.firebase.firestore.IgnoreExtraProperties
+import com.google.firebase.firestore.ServerTimestamp
+import java.util.Date
+
+@IgnoreExtraProperties
+data class UserData (
+    // Id of the user
+    var id: String = "",
+    // The username the user is registred with
+    var username: String = "",
+    // The email the user is registred with
+    var email: String = "",
+    // Encrypted password
+    var password: String = "",
+    // Location of the user
+    var location: String = "",
+    // Date the user registered
+    @ServerTimestamp
+    var dateRegistered: Date? = null, //Retireves the timestamp from firebase
+    // Url to their profile picture. Url leads to Firestore
+    var profilePictureUrl: String= "",
+    // List of adId's
+    var favorites: List<String> = emptyList(),
+    // Type of user
+    var userType: UserTypeEnum = UserTypeEnum.REGULAR //Default usertype is regular
+)
+{}
