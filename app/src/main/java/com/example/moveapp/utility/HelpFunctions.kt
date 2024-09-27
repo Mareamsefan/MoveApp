2e69eae6bf91c9cbd6f5cc875a705e31770c4698
Vebhamnvik <vebjornhamnvik95@gmail.com>
2024-09-27 12:21:34 +0200

    Added password validation

--- app/src/main/java/com/example/moveapp/utility/HelpFunctions.kt
+++ app/src/main/java/com/example/moveapp/utility/HelpFunctions.kt
@@ -1,20 +1,38 @@
 package com.example.moveapp.utility
 import java.security.MessageDigest
+import android.content.Context
+import android.widget.Toast
 
 class HelpFunctions {
 
     companion object {
-        fun passwordEncryptor(argument: String): String {
-            val hashed = MessageDigest.getInstance("SHA-256").digest(argument.toByteArray())
-            return hashed.joinToString("") {"%02x".format(it)}
+        fun validatePassword(context: Context, password: String): Boolean {
+            val specialCharsRegex = Regex("[^A-Za-z0-9]")
+            val numbersRegex = Regex("[0-9]")
+
+            // Checking length of password
+            if (password.length <= 7) {
+                Toast.makeText(context, "Password must be longer than 7 characters", Toast.LENGTH_SHORT).show()
+                return false
+            }
+
+            // Checking if there is any special characters in the password
+            if (!specialCharsRegex.containsMatchIn(password)) {
+                Toast.makeText(context, "Password must contain at least one special character", Toast.LENGTH_SHORT).show()
+                return false
+            }
+
+            // Checking if there are any numbers in the password
+            if (!numbersRegex.containsMatchIn(password)) {
+                Toast.makeText(context, "Password must contain at least one number", Toast.LENGTH_SHORT).show()
+                return false
+            }
+            // Nice password :)
+            return true
         }
 
-        fun comparePasswords(typedPassword: String, existingPassword: String): Boolean {
-            // Hashes the password the user typed in
-            val hashed = passwordEncryptor(typedPassword)
 
-            // Compares it ith the hashed passord from the database
-            return (hashed == existingPassword)
+
         }
     }
 
