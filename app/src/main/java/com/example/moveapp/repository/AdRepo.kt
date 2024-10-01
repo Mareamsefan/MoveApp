068d1b86d6d461ca033a28be9686e7c2701df5c9
Vebhamnvik <vebjornhamnvik95@gmail.com>
2024-10-01 10:41:52 +0200

    creating ad

--- app/src/main/java/com/example/moveapp/repository/AdRepo.kt
+++ app/src/main/java/com/example/moveapp/repository/AdRepo.kt
@@ -8,96 +8,99 @@ class AdRepo {
 
     // FUNCTION TO CREATE INITIAL AD
 
-    suspend fun addAdToDatabase(ad: AdData): Boolean {
-        return try {
-            FirestoreService.getAdsCollection().document(ad.adId).set(ad).await()
-            true
-        } catch (e: Exception) {
-            e.printStackTrace()
-            false
+    companion object{
+        suspend fun addAdToDatabase(ad: AdData): Boolean {
+            return try {
+                ad.adId?.let { FirestoreService.getAdsCollection().document(it).set(ad).await() }
+                true
+            } catch (e: Exception) {
+                e.printStackTrace()
+                false
+            }
+        }
+
+        // FUNCTIONS TO UPDATE EXISTING ADS
+
+        suspend fun updateAdTitleInDatabase(adId: String, newTitle: String): Boolean {
+            return try {
+                // Retrieves the ad document by adId
+                val adDocumentReference = FirestoreService.getAdsCollection().document(adId)
+                // Replaces old adTitle with newTitle
+                adDocumentReference.update("adTitle", newTitle).await()
+                true
+            } catch (e: Exception) {
+                e.printStackTrace()
+                false
+            }
+        }
+
+        suspend fun updateAdImagesInDatabase(adId: String, newImages: List<String>): Boolean {
+            return try {
+                // Retrieves the ad document by adId
+                val adDocumentReference = FirestoreService.getAdsCollection().document(adId)
+                // Replaces old adImages with newImages
+                adDocumentReference.update("adImages", newImages).await()
+                true
+            } catch (e: Exception) {
+                e.printStackTrace()
+                false
+            }
+        }
+
+        suspend fun updateAdPriceInDatabase(adId: String, newPrice: Double): Boolean {
+            return try {
+                // Retrieves the ad document by adId
+                val adDocumentReference = FirestoreService.getAdsCollection().document(adId)
+                // Replaces old adPrice with newPrice
+                adDocumentReference.update("adPrice", newPrice).await()
+                true
+            } catch (e: Exception) {
+                e.printStackTrace()
+                false
+            }
+        }
+
+        suspend fun updateAdCategoryInDatabase(adId: String, newCategory: String): Boolean {
+            return try {
+                // Retrieves the ad document by adId
+                val adDocumentReference = FirestoreService.getAdsCollection().document(adId)
+                // Replaces old adCategory with newCategory
+                adDocumentReference.update("adCategory", newCategory).await()
+                true
+            } catch (e: Exception) {
+                e.printStackTrace()
+                false
+            }
+        }
+
+        suspend fun updateAdDescriptionInDatabase(adId: String, newDescription: String): Boolean {
+            return try {
+                // Retrieves the ad document by adId
+                val adDocumentReference = FirestoreService.getAdsCollection().document(adId)
+                // Replaces old adDescription with newDescription
+                adDocumentReference.update("adDescription", newDescription).await()
+                true
+            } catch (e: Exception) {
+                e.printStackTrace()
+                false
+            }
+        }
+
+        // FUNCTION TO MAKE AD INACTIVE
+
+        suspend fun setAddInactive(adId: String): Boolean {
+            return try {
+                // Retrieves the ad document by adId
+                val adDocumentReference = FirestoreService.getAdsCollection().document(adId)
+                // sets isActive to be false
+                adDocumentReference.update("isActive", false).await()
+                true
+            } catch (e: Exception) {
+                e.printStackTrace()
+                false
+            }
         }
     }
 
-    // FUNCTIONS TO UPDATE EXISTING ADS
-
-    suspend fun updateAdTitleInDatabase(adId: String, newTitle: String): Boolean {
-        return try {
-            // Retrieves the ad document by adId
-            val adDocumentReference = FirestoreService.getAdsCollection().document(adId)
-            // Replaces old adTitle with newTitle
-            adDocumentReference.update("adTitle", newTitle).await()
-            true
-        } catch (e: Exception) {
-            e.printStackTrace()
-            false
-        }
-    }
-
-    suspend fun updateAdImagesInDatabase(adId: String, newImages: List<String>): Boolean {
-        return try {
-            // Retrieves the ad document by adId
-            val adDocumentReference = FirestoreService.getAdsCollection().document(adId)
-            // Replaces old adImages with newImages
-            adDocumentReference.update("adImages", newImages).await()
-            true
-        } catch (e: Exception) {
-            e.printStackTrace()
-            false
-        }
-    }
-
-    suspend fun updateAdPriceInDatabase(adId: String, newPrice: Double): Boolean {
-        return try {
-            // Retrieves the ad document by adId
-            val adDocumentReference = FirestoreService.getAdsCollection().document(adId)
-            // Replaces old adPrice with newPrice
-            adDocumentReference.update("adPrice", newPrice).await()
-            true
-        } catch (e: Exception) {
-            e.printStackTrace()
-            false
-        }
-    }
-
-    suspend fun updateAdCategoryInDatabase(adId: String, newCategory: String): Boolean {
-        return try {
-            // Retrieves the ad document by adId
-            val adDocumentReference = FirestoreService.getAdsCollection().document(adId)
-            // Replaces old adCategory with newCategory
-            adDocumentReference.update("adCategory", newCategory).await()
-            true
-        } catch (e: Exception) {
-            e.printStackTrace()
-            false
-        }
-    }
-
-    suspend fun updateAdDescriptionInDatabase(adId: String, newDescription: String): Boolean {
-        return try {
-            // Retrieves the ad document by adId
-            val adDocumentReference = FirestoreService.getAdsCollection().document(adId)
-            // Replaces old adDescription with newDescription
-            adDocumentReference.update("adDescription", newDescription).await()
-            true
-        } catch (e: Exception) {
-            e.printStackTrace()
-            false
-        }
-    }
-
-    // FUNCTION TO MAKE AD INACTIVE
-
-    suspend fun setAddInactive(adId: String): Boolean {
-        return try {
-            // Retrieves the ad document by adId
-            val adDocumentReference = FirestoreService.getAdsCollection().document(adId)
-            // sets isActive to be false
-            adDocumentReference.update("isActive", false).await()
-            true
-        } catch (e: Exception) {
-            e.printStackTrace()
-            false
-        }
-    }
 
 }
--- app/src/main/java/com/example/moveapp/viewModel/AdViewModel.kt
+++ app/src/main/java/com/example/moveapp/viewModel/AdViewModel.kt
@@ -1,4 +1,32 @@
 package com.example.moveapp.viewModel
+import AdData
+import android.content.Context
+import android.widget.Toast
+import com.example.moveapp.repository.AdRepo
+
 
 class AdViewModel {
+
+    companion object {
+
+        suspend fun createAd(context: Context, adTitle: String, adPrice: Double, adCategory: String, adDescription: String, userId: String, address: String, postalCode: String): Boolean {
+            return try {
+                val ad = AdData(adTitle=adTitle, adPrice=adPrice, adCategory= adCategory, adDescription=adDescription, userId = userId, address= address, postalCode = postalCode)
+
+                val success = AdRepo.addAdToDatabase(ad)
+
+                if (!success) {
+                    Toast.makeText(context, "Adding the ad to the database failed!", Toast.LENGTH_SHORT).show()
+                    return false
+                }
+
+                true
+            } catch (e: Exception) {
+                Toast.makeText(context, "Ad creation failed, please try again!", Toast.LENGTH_SHORT).show()
+                false
+
+            }
+        }
+
+    }
 }
