ef9d8df0420a371b45f43e2a509dcda985c4bbb0
Vebhamnvik <vebjornhamnvik95@gmail.com>
2024-10-01 11:34:41 +0200

    db pls

--- app/src/main/java/com/example/moveapp/MainActivity.kt
+++ app/src/main/java/com/example/moveapp/MainActivity.kt
@@ -6,10 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.example.moveapp.ui.navigation.AppNavigation
 import com.example.moveapp.ui.theme.MoveAppTheme
+import com.example.moveapp.utility.FirestoreService
+import android.util.Log
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        FirestoreService.getAdsCollection()
+            .limit(1) // Just fetching one document to trigger Firestore initialization
+            .get()
+            .addOnSuccessListener {
+                Log.d("MainActivity", "Successfully connected to Firestore!")
+            }
+            .addOnFailureListener {
+                Log.e("MainActivity", "Failed to connect to Firestore", it)
+            }
+
         enableEdgeToEdge()
         setContent {
             MoveAppTheme{
