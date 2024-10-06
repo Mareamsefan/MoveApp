ef9b0ebf6fcc5e1bb31d180249e9e97e5bfe4703
Fiskesprell <111729570+fiskesprell@users.noreply.github.com>
2024-10-06 13:57:08 +0200

    start av endrePassord

---
+++ app/src/main/java/com/example/moveapp/ui/screens/profile/ChangePasswordScreen.kt
@@ -0,0 +1,21 @@
+package com.example.moveapp.ui.screens.profile
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+
+@Composable
+fun ChangePasswordScreen(navController: NavController) {
+    // Make box for old password
+
+    // Make box for new password
+
+    // Make second box for new password
+
+    // Make submit button
+
+    // Check if old password is correct
+
+    // Check if
+
+}
