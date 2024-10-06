f21142d800ca0bcf426f2884dac71f7e275a184e
kosovarei <111733299+kosovarei@users.noreply.github.com>
2024-10-06 15:08:05 +0200

    made a map that takes the amulators current location and puts it in the map, if location permission is allowed. if location is not allowed it will put a default location for oslo

--- app/src/main/java/com/example/moveapp/ui/screens/map/MapScreen.kt
+++ app/src/main/java/com/example/moveapp/ui/screens/map/MapScreen.kt
@@ -1,6 +1,8 @@
 package com.example.moveapp.ui.screens.map
 
 import android.app.Activity
+import android.util.Log
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -10,6 +12,7 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.navigation.NavController
+import com.example.moveapp.utility.LocationUtil
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 
@@ -18,60 +21,98 @@ import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
 
 
-@Composable
 
+@Composable
 fun MapScreen(navController: NavController) {
+    val context = LocalContext.current
+    var userLocation by remember { mutableStateOf<GeoPoint?>(GeoPoint(59.9139, 10.7522)) }
+    val locationUtil = LocationUtil()
+    var hasLocationPermission by remember { mutableStateOf(false) }
+
+
+
+
+    locationUtil.getUserLocation(context) { location ->
+        location?.let {
+            userLocation = it
+            hasLocationPermission = true
+            Log.d("MapScreen", "User location retrieved: ${it.latitude}, ${it.longitude}")
+        } ?: run {
+            Log.d("MapScreen", "Failed to retrieve user location")
+        }
+    }
+
+
+    Log.d("MapScreen", "user location on map ${userLocation}")
+
+
+
+    if (hasLocationPermission){
+        Map(userLocation!!, hasLocationPermission)
+    }
+}
+
+@Composable
+fun Map(geoPoint: GeoPoint, hasLocationPermission: Boolean) {
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
 
-    Configuration.getInstance().load(context, context.getSharedPreferences("osmdroid", Activity.MODE_PRIVATE))
+    Configuration.getInstance()
+        .load(context, context.getSharedPreferences("osmdroid", Activity.MODE_PRIVATE))
 
     var mapView: MapView? by remember { mutableStateOf(null) }
 
-    AndroidView(
-        modifier = Modifier.fillMaxSize(),
-        factory = { context ->
-            MapView(context).apply {
-                mapView = this
-                setTileSource(TileSourceFactory.MAPNIK)
+    Box(modifier = Modifier) {
+        AndroidView(
+            modifier = Modifier.fillMaxSize(),
+            factory = { context ->
+                MapView(context).apply {
+                    mapView = this
+                    setTileSource(TileSourceFactory.MAPNIK)
 
-                setMultiTouchControls(true)
+                    setMultiTouchControls(true)
 
-                controller.setZoom(15.0)
-                controller.setCenter(GeoPoint(48.8583, 2.2944)) // Eiffel Tower
-
-                val marker = Marker(this).apply {
-                    position = GeoPoint(48.8583, 2.2944)
-                    setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-                    title = "Eiffel Tower"
+                    controller.setZoom(15.0)
+                    controller.setCenter(geoPoint)
+                    val marker = Marker(this).apply {
+                        position = geoPoint
+                        Log.d("MapScreen", "user location on map ${geoPoint}")
+                        setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+                        title = "My location"
+                    }
+                    if (hasLocationPermission)
+                        overlays.add(marker)
                 }
-                overlays.add(marker)
+            },
+            update = { mapView?.invalidate() }
+        )
+
+        DisposableEffect(lifecycleOwner) {
+            val observer = LifecycleEventObserver { _, event ->
+                when (event) {
+                    Lifecycle.Event.ON_RESUME -> {
+                        mapView?.onResume()
+                    }
+
+                    Lifecycle.Event.ON_PAUSE -> {
+                        mapView?.onPause()
+                    }
+
+                    Lifecycle.Event.ON_DESTROY -> {
+                        mapView?.onDetach()
+                    }
+
+                    else -> {}
+                }
             }
-        },
-        update = { mapView?.invalidate() }
-    )
+            lifecycleOwner.lifecycle.addObserver(observer)
 
-    DisposableEffect(lifecycleOwner) {
-        val observer = LifecycleEventObserver { _, event ->
-            when (event) {
-                Lifecycle.Event.ON_RESUME -> {
-                    mapView?.onResume()
-                }
-                Lifecycle.Event.ON_PAUSE -> {
-                    mapView?.onPause()
-                }
-                Lifecycle.Event.ON_DESTROY -> {
-                    mapView?.onDetach()
-                }
-                else -> {}
+            onDispose {
+                lifecycleOwner.lifecycle.removeObserver(observer)
+                mapView?.onDetach()
             }
         }
-        lifecycleOwner.lifecycle.addObserver(observer)
 
-        onDispose {
-            lifecycleOwner.lifecycle.removeObserver(observer)
-            mapView?.onDetach()
-        }
     }
 }
 
--- app/src/main/java/com/example/moveapp/utility/LocationUtil.kt
+++ app/src/main/java/com/example/moveapp/utility/LocationUtil.kt
@@ -1,15 +1,86 @@
 package com.example.moveapp.utility
 
-import android.os.Bundle
+import android.Manifest
+import android.content.Context
+import android.util.Log
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationTokenSource
+import org.osmdroid.util.GeoPoint
 
 class LocationUtil {
 
+    @OptIn(ExperimentalPermissionsApi::class)
     @Composable
-    fun User_location(savedInstanceState: Bundle?) {
-        // method that takes users current location
+    fun RequestUserLocation(
+        onPermissionGranted: () -> Unit,
+        onPermissionDenied: () -> Unit,
+        onPermissionsRevoked: () -> Unit
+    ) {
+        val permissionState = rememberMultiplePermissionsState(
+            listOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            )
+        )
+
+        LaunchedEffect(Unit) {
+            // Launch permission request
+            if (!permissionState.allPermissionsGranted) {
+                permissionState.launchMultiplePermissionRequest()
+            }
+        }
+
+        // Check permission states after request
+        if (permissionState.permissions.all { it.status.isGranted }) {
+            onPermissionGranted()
+        } else if (permissionState.permissions.all { !it.status.isGranted }) {
+            onPermissionDenied()
+        } else {
+            onPermissionsRevoked()
+        }
+    }
+
+    @Composable
+    fun getUserLocation(context: Context, onLocationResult: (GeoPoint?) -> Unit) {
+
+        RequestUserLocation(
+            onPermissionGranted = {
+                Log.d("MapScreen", "Location permissions given")
+
+                val fusedLocationClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+
+                val accuracy = Priority.PRIORITY_HIGH_ACCURACY
+
+                fusedLocationClient.getCurrentLocation(
+                    accuracy, CancellationTokenSource().token,
+                ).addOnSuccessListener { location ->
+                    location?.let {
+                        val geoPoint = GeoPoint(location.latitude, location.longitude)
+                        onLocationResult(geoPoint)
+                    }
+                }.addOnFailureListener { exception ->
+                    Log.d("LocationUtil", "Last known location is null")
+                    onLocationResult(null)
+                }
+            },
+            onPermissionDenied = {
+                Log.d("MapScreen", "Location permission denied")
+                onLocationResult(GeoPoint(59.9139, 10.7522))
+            },
+            onPermissionsRevoked = {
+                Log.d("MapScreen", "Location permissions revoked")
+                onLocationResult(GeoPoint(59.9139, 10.7522))
+            }
+        )
+
     }
 }
 
-// https://developer.android.com/develop/sensors-and-location/location/permissions
-//
+// https://medium.com/@munbonecci/how-to-get-your-location-in-jetpack-compose-f085031df4c1
