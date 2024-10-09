1f1ac31b69db98d99bc987b32fcb872ee65a35c1
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2024-10-09 16:03:38 +0200

    Fixed the theme so that the colors show on the app.

--- app/src/main/java/com/example/moveapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/moveapp/ui/theme/Theme.kt
@@ -259,7 +259,7 @@ val unspecified_scheme = ColorFamily(
 fun MoveAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable() () -> Unit
 ) {
   val colorScheme = when {
