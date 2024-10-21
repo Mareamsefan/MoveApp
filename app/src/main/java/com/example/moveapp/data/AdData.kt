6283bfb253ae1049d59d7fe9fe9708bd0b1d01bc
kosovarei <111733299+kosovarei@users.noreply.github.com>
2024-10-21 12:48:10 +0200

    added city to AdData

--- app/src/main/java/com/example/moveapp/data/AdData.kt
+++ app/src/main/java/com/example/moveapp/data/AdData.kt
@@ -11,17 +11,19 @@ import java.util.UUID
 data class AdData(
     var adId: String? = UUID.randomUUID().toString(),           // Unique identifier for the ad
     var adTitle: String = "",                                   // Title or description of the ad
-    var adPrice: Double = 0.0,                                 // Price for the item or service
+    var adPrice: Double = 0.0,                                  // Price for the item or service
     var adCategory: String = "",                                // Category (e.g., 'Truck Rental', 'Item Sale', 'Moving Service')
     var adDescription: String = "",                             // Detailed description of the ad
-    var userId: String = "",                                   // ID of the user who posted the ad
+    var userId: String = "",                                    // ID of the user who posted the ad
     var isActive: Boolean = true,                               // The ad is only shown in the app if it is active.
-    var country: String = "Norway",                            // Default country
+    var country: String = "Norway",                             // Default country
     var address: String = "",                                   // Address where the ad is located
-    var postalCode: String = "",                                // Postal code of the ad's location
+    var postalCode: String = "",
+    // TODO: add city column to the database
+    var city: String = "",
     @ServerTimestamp
-    var timestamp: Date? = null,                               // Timestamp for when the ad was posted
-    var adImages: List<String> = emptyList()                   // List of image URLs for the ad (if any)
+    var timestamp: Date? = null,                                // Timestamp for when the ad was posted
+    var adImages: List<String> = emptyList()                    // List of image URLs for the ad (if any)
 ) {
     // Default constructor required for Firestore
     constructor() : this(
@@ -33,6 +35,7 @@ data class AdData(
         userId = "",
         isActive = true,
         country = "Norway",
+        city = "",
         address = "",
         postalCode = "",
         timestamp = null,
