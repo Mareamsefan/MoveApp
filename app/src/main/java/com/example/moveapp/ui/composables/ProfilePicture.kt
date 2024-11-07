dc5edc036d57964c7ed4fe7c73ff4141c6a22fe5
kosovarei <111733299+kosovarei@users.noreply.github.com>
2024-11-07 14:50:08 +0100

    fixed profile picure with its own display

--- app/src/main/java/com/example/moveapp/ui/composables/ProfilePicture.kt
+++ app/src/main/java/com/example/moveapp/ui/composables/ProfilePicture.kt
@@ -1,39 +1,34 @@
 package com.example.moveapp.ui.composables
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Card
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 
-
 @Composable
-fun ProfilePicture(image: String){
+fun ProfilePicture(imageState: MutableState<String>) {
+    val image = imageState.value
     if (image.isEmpty()) return
-    Box(modifier = Modifier.height(300.dp)){
+
+    Box(modifier = Modifier.height(300.dp)) {
+        Card(modifier = Modifier.fillMaxWidth()) {
             val painter = rememberAsyncImagePainter(model = image)
-            Card {
-                Image(
-                    painter = painter,
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxWidth()
-                        .height(230.dp)
-                        .padding(8.dp)
-                )
-            }
+
+            Image(
+                painter = painter,
+                contentDescription = "Profile Picture",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(230.dp)
+                    .padding(8.dp)
+            )
+        }
     }
 }
--- app/src/main/java/com/example/moveapp/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/moveapp/ui/screens/profile/ProfileScreen.kt
@@ -2,24 +2,18 @@ package com.example.moveapp.ui.screens.profile
 
 import android.annotation.SuppressLint
 import android.net.Uri
-import android.util.Log
-import androidx.compose.foundation.gestures.detectTransformGestures
 
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -27,16 +21,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.rememberNestedScrollInteropConnection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -45,15 +36,12 @@ import com.example.moveapp.R
 import com.example.moveapp.data.AdData
 import com.example.moveapp.data.UserData
 import com.example.moveapp.repository.AdRepo
-import com.example.moveapp.repository.UserRepo
 import com.example.moveapp.ui.composables.AdItem
-import com.example.moveapp.ui.composables.Image_swipe
 import com.example.moveapp.ui.composables.ProfilePicture
 import com.example.moveapp.utility.FireAuthService.getCurrentUser
 import com.example.moveapp.utility.FireAuthService.getUsername
 import com.example.moveapp.utility.FirestoreService.readDocument
 import com.example.moveapp.viewModel.UserViewModel.Companion.uploadAndSetUserProfilePicture
-import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 @SuppressLint("CoroutineCreationDuringComposition")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -110,6 +98,7 @@ fun Profile(navController: NavController) {
                 coroutineScope.launch {
                     if (userId != null) {
                         val success = uploadAndSetUserProfilePicture(userId, it)
+                        profileImageUrl.value = it.toString()
                         if (success) {
                             errorMessage.value = "Image uploaded successfully."
                         } else {
@@ -141,10 +130,8 @@ fun Profile(navController: NavController) {
                 style = MaterialTheme.typography.titleLarge,
                 modifier = Modifier.padding(vertical = 16.dp)
             )
-
             // Profile image
-            ProfilePicture(image = profileImageUrl.value)
-            //Image_swipe(imageList = listOf(profileImageUrl.value))
+            ProfilePicture(imageState = profileImageUrl )
 
             // Upload or update image button
             Button(
