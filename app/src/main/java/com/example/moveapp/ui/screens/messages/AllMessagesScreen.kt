89e26eb0d99a1e0148f9d50b9329ab3b14d9bd52
Mareamsefan <111729393+Mareamsefan@users.noreply.github.com>
2024-11-12 15:14:16 +0100

    fixed layout of messages.

--- app/src/main/java/com/example/moveapp/ui/screens/messages/AllMessagesScreen.kt
+++ app/src/main/java/com/example/moveapp/ui/screens/messages/AllMessagesScreen.kt
@@ -3,7 +3,6 @@ import ChatItemWithAd
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.pulltorefresh.PullToRefreshBox
@@ -89,7 +88,7 @@ fun AllMessagesScreen(navController: NavController) {
             PullToRefreshBox(
                 modifier = Modifier
                     .fillMaxSize(),
-                contentAlignment = Alignment.Center,
+                contentAlignment = Alignment.TopEnd,
                 state = refreshState,
                 isRefreshing = isRefreshing,
                 onRefresh = {
