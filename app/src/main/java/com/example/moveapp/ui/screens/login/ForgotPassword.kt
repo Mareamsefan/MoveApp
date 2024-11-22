e08e9b34bdec9fe1d45605131a9e63ff411d1899
Fiskesprell <111729570+fiskesprell@users.noreply.github.com>
2024-11-22 11:55:37 +0100

    Finished ForgotPassword page - untested.

--- app/src/main/java/com/example/moveapp/ui/screens/login/ForgotPassword.kt
+++ app/src/main/java/com/example/moveapp/ui/screens/login/ForgotPassword.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -16,11 +20,14 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.moveapp.R
 import com.example.moveapp.utility.FireAuthService.sendUserPasswordResetEmail
+import com.example.moveapp.viewModel.UserViewModel.Companion.validateEmail
 
 @Composable
 fun ForgotPassword(navController: NavController) {
     val email = remember { mutableStateOf("") }
+    var showErrorDialog by remember { mutableStateOf(false) }
     val errorMessage = remember { mutableStateOf("") }
+    var dialogMessage by remember { mutableStateOf("") }
 
     Box(
         modifier = Modifier.fillMaxSize(),
@@ -34,11 +41,16 @@ fun ForgotPassword(navController: NavController) {
         ) {
             // --- Send Password Reset Email ---
             Button(onClick = {
-                val email = userEmail.value
+                val emailToSendRequestTo = email.value
 
-                if (email.isNotEmpty()){
-                    sendUserPasswordResetEmail(email)
-                    errorMessage.value = "Email sent! If you do not see the email shortly, please check your spam folder."
+                if (emailToSendRequestTo.isEmpty() or !validateEmail(emailToSendRequestTo)){
+                    errorMessage.value = "Please enter a valid email address."
+                }
+
+                if (emailToSendRequestTo.isNotEmpty() and validateEmail(emailToSendRequestTo)){
+                    sendUserPasswordResetEmail(emailToSendRequestTo)
+                    errorMessage.value = "We have sent an email to ${emailToSendRequestTo}." +
+                            " If you do not see the email shortly, please check your spam folder."
                 } else {
                     errorMessage.value = "Something went wrong. Please wait a few seconds and try again."
                 }
@@ -48,5 +60,26 @@ fun ForgotPassword(navController: NavController) {
             }
         }
 
+    }
 
+    // Handling Error Messages & Notices
+    LaunchedEffect(errorMessage.value) {
+        if (errorMessage.value.isNotEmpty()) {
+            dialogMessage = errorMessage.value
+            showErrorDialog = true
+        }
+    }
+
+    if (showErrorDialog) {
+        AlertDialog(
+            onDismissRequest = { showErrorDialog = false },
+            title = { Text("Notice") },
+            text = { Text(dialogMessage) },
+            confirmButton = {
+                Button(onClick = { showErrorDialog = false }) {
+                    Text("OK")
+                }
+            }
+        )
+    }
 }
