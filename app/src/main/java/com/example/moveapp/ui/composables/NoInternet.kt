ad9592692ee0209a0d76351458b76f312a4fb6c2
kosovarei <111733299+kosovarei@users.noreply.github.com>
2024-11-24 23:22:27 +0100

    no internet connection message on all screens. when internet is connected you can navigate without problem

---
+++ app/src/main/java/com/example/moveapp/ui/composables/NoInternet.kt
@@ -0,0 +1,39 @@
+package com.example.moveapp.ui.composables
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.WifiOff
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NoInternet(){
+    Box() {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = Icons.Filled.WifiOff,
+                contentDescription = "No Internet",
+                modifier = Modifier.size(64.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "No Internet Connection")
+        }
+    }
+}
