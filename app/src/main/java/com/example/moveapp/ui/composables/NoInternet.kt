5a0a9c5ad82adb87ee3397455d01c1dd82643ab9
kosovarei <111733299+kosovarei@users.noreply.github.com>
2024-11-25 11:41:04 +0100

    refresh button they can test if internet came back, if adID/chatID didnt get saved they get navigated back with error message

--- app/src/main/java/com/example/moveapp/ui/composables/NoInternet.kt
+++ app/src/main/java/com/example/moveapp/ui/composables/NoInternet.kt
@@ -10,15 +10,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.WifiOff
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.moveapp.R
 
 @Composable
-fun NoInternet(){
+fun NoInternet(refresh: () -> Unit){
     Box() {
         Column(
             modifier = Modifier
@@ -34,6 +37,12 @@ fun NoInternet(){
             )
             Spacer(modifier = Modifier.height(16.dp))
             Text(text = "No Internet Connection")
+            Button(
+                onClick = { refresh()},
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(stringResource(R.string.try_again))
+            }
         }
     }
 }
