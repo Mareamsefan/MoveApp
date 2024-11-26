719ac25d7e3edc1c8ef4992a95e19743086e8895
kosovarei <111733299+kosovarei@users.noreply.github.com>
2024-11-26 23:21:46 +0100

    choose dark theme/ light theme in settings

--- app/src/main/java/com/example/moveapp/ui/screens/profile/ProfileSettingsScreen.kt
+++ app/src/main/java/com/example/moveapp/ui/screens/profile/ProfileSettingsScreen.kt
@@ -1,11 +1,9 @@
 package com.example.moveapp.ui.screens.profile
+
+import android.app.Activity
 import android.content.Intent
 import android.provider.Settings
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,25 +14,51 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.moveapp.R
+import com.example.moveapp.utility.PreferencesHelper
 
 @Composable
 fun ProfileSettingsScreen(navController: NavController) {
     val context = LocalContext.current
-    Box(modifier = Modifier.padding(20.dp)){
-        Column(){
-            Row(verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(8.dp)){
-                Text(stringResource(R.string.revoke_permissions))
+
+    Box(modifier = Modifier.padding(20.dp)) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+
+            Text(stringResource(R.string.revoke_permissions))
+            Button(onClick = {
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                val uri = android.net.Uri.fromParts("package", context.packageName, null)
+                intent.data = uri
+                context.startActivity(intent)
+            }) {
+                Text(stringResource(R.string.lead_me_to_settings))
+            }
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(stringResource(R.string.choose_a_theme))
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
                 Button(onClick = {
-                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                    val uri = android.net.Uri.fromParts("package", context.packageName, null)
-                    intent.data = uri
-                    context.startActivity(intent)
+                    PreferencesHelper.saveThemeMode(context, false)
+                    (context as? Activity)?.recreate()
                 }) {
-                    Text(stringResource(R.string.lead_me_to_settings))
+                    Text(stringResource(R.string.light_theme))
+                }
+
+                Button(onClick = {
+                    PreferencesHelper.saveThemeMode(context, true)
+                    (context as? Activity)?.recreate()
+                }) {
+                    Text(stringResource(R.string.dark_theme))
                 }
             }
         }
     }
-
 }
